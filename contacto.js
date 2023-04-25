1e73e16715541d6648714cc9d9d78acc2dd3ba94
ezebarba <ezebarba@gmail.com>
2023-04-25 20:45:15 -0300

    Presentacion

--- contacto.js
+++ contacto.js
@@ -1,86 +1,90 @@
-/* import { jsPDF } from "jspdf"; */
-
-$(document).ready(function() {
-  event.preventDefault();
-    // Validación del formulario usando Validate.js
-    $('#contact-form').validate({
-      rules: {
-        name: {
-          required: true
-        },
-        email: {
-          required: true,
-          email: true
-        },
-        subject: {
-          required: true
-        },
-        message: {
-          required: true
-        }
-      },
-      messages: {
-        name: {
-          required: 'Por favor ingresa tu nombre completo.'
-        },
-        email: {
-          required: 'Por favor ingresa tu correo electrónico.',
-          email: 'Por favor ingresa un correo electrónico válido.'
-        },
-        subject: {
-          required: 'Por favor ingresa el asunto del mensaje.'
-        },
-        message: {
-          required: 'Por favor escribe tu mensaje.'
-        }
-      },
-      errorElement: 'div',
-      errorPlacement: function(error, element) {
-        error.addClass('invalid-feedback');
-        element.closest('.form-group').append(error);
-      },
-      highlight: function(element, errorClass, validClass) {
-        $(element).addClass('is-invalid');
-      },
-      unhighlight: function(element, errorClass, validClass) {
-        $(element).removeClass('is-invalid');
-      }
-    });
+$("#contact-form").submit(function(event) {
+  event.preventDefault(); // Evita que la página se recargue
+  alert('Se envio correctamente el formulario')
 });
 
-// Manejar el envío del formulario
-$("form[name='contact-form']").submit(function(event) {
-    // Prevenir el comportamiento predeterminado del formulario
-  
-    // Verificar si el formulario es válido
-    if ($(this).valid()) {
-      // Obtener los datos del formulario
-      var fullname = $("input[name='fullname']").val();
-      var email = $("input[name='email']").val();
-      var subject = $("input[name='subject']").val();
-      var message = $("textarea[name='message']").val();
-  
-      // Crear una nueva instancia de jsPDF
-      var doc = new jsPDF();
-  
-      // Establecer el tamaño de fuente
-      doc.setFontSize(12);
-  
-      // Agregar contenido al PDF
-      doc.text("Nombre y Apellido: " + fullname, 10, 20);
-      doc.text("Email: " + email, 10, 30);
-      doc.text("Asunto: " + subject, 10, 40);
-      doc.text("Mensaje: " + message, 10, 50);
-  
-      // Descargar el PDF
-      doc.save("formulario_contacto.pdf");
+$(document).ready(function() {
+  $('#contact-form').validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 2
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      subject: {
+        required: true,
+        minlength: 2
+      },
+      message: {
+        required: true,
+        minlength: 10
+      }
+    },
+    messages: {
+      name: {
+        required: 'Por favor, ingrese su nombre',
+        minlength: 'El nombre debe tener al menos 2 caracteres'
+      },
+      email: {
+        required: 'Por favor, ingrese su correo electrónico',
+        email: 'Por favor, ingrese un correo electrónico válido'
+      },
+      subject: {
+        required: 'Por favor, ingrese un asunto válido',
+        minlength: 'El asunto debe tener al menos 2 caracteres'
+      },
+      message: {
+        required: 'Por favor, ingrese un mensaje válido',
+        minlength: 'El asunto debe tener al menos 10 caracteres'        
+      }
+    },
+    submitHandler: function(form) {
+      // Enviar el formulario si es válido
+      $.ajax({
+        type: 'POST',
+        url: 'send.php',
+        data: $(form).serialize(),
+        success: function(response) {
+          alert('¡Gracias por contactarnos! Nos pondremos en contacto con usted lo antes posible.');
+          form.reset();
+        },
+        error: function(response) {
+          alert('Ha ocurrido un error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.');
+        }
+      });
     }
   });
-  
+});
 
-  document.addEventListener('DOMContentLoaded', function() {
-    let pdf = new jsPDF('p', 'pt', 'a4');
-    pdf.addHTML(document.querySelector('#pdf'), function() {
-        pdf.save('documentoPDF1.pdf');
+
+$(document).ready(function() {
+  // ... Código del formulario de contacto ...
+  
+  // Listener para el botón de descarga
+  $('#download-pdf').on('click', function() {
+    // Obtener la información del formulario
+    var form = $('#contact-form')[0];
+    var opt = {
+      margin:       1,
+      filename:     'contacto.pdf',
+      image:        { type: 'jpeg', quality: 0.98 },
+      html2canvas:  { scale: 2 },
+      jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    var formData = new FormData(form);
+    var data = {};
+    formData.forEach(function(value, key) {
+      data[key] = value;
     });
+
+    // Generar el PDF
+    html2pdf().set(opt).from($('#contact-form').get(0)).save();
+
+    // Evitar que se recargue la página
+    return false;
+  });
 });
